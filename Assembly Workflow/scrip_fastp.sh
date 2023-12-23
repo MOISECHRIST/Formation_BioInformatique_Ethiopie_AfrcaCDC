07c79390a15654fa52089b1afd770d24bae98f6b
MOISE CHRISTIAN JUNIOR MEKA <115233333+MOISECHRIST@users.noreply.github.com>
2023-12-23 15:13:18 +0100

    Update scrip_fastp.sh

--- Assembly Workflow/scrip_fastp.sh
+++ Assembly Workflow/scrip_fastp.sh
@@ -1,3 +1,15 @@
+echo "1- Quality control of raw data"
+
+echo "Fastqc"
+
+fastqc reads/*qc*.fastq.gz -o quality_control/fastqc/
+
+echo "Multiqc"
+
+multiqc quality_control/fastqc/ -o quality_control/multiqc/before/
+
+echo "2- Trimming and preprocessing of raw data"
+
 echo "Fastp"
 files=$(ls reads/ | cut -d_ -f1,2 | sort | uniq)
 for file in $files
@@ -5,6 +17,9 @@ do
 echo "$file"
 fastp -i reads/"$file"_L001_R1_001.fastq.gz -I reads/"$file"_L001_R2_001.fastq.gz -n 1 -f 15 -F15 --detect_adapter_for_pe -o quality_control/fastp/"$file"_qc_R1.fastq.gz -O quality_control/fastp/"$file"_qc_R2.fastq.gz -h quality_control/fastp/"$file".html -j quality_control/fastp/"$file".json
 done
+
+
+echo "3- Quality control of cleaned data"
 
 echo "Fastqc"
 
